3c42cadb233d8d9d0700e14902472d23f3e7a812
NivedhaSenthil <nivedhasenthil@gmail.com>
2017-08-18 11:06:42 +0530

    Get username and password from command prompt

--- releasenote/__init__.py
+++ releasenote/__init__.py
@@ -8,9 +8,12 @@ import mingle
 @click.command()
 @click.option('--lastcommit', '-lc', default="", help="Commit hash of last release")
 @click.option('--committill', '-ct', default="", help="Commit hash of till which notes has to generated")
-def get_release_note(lastcommit,committill):
+@click.option('--project', prompt='Project Name',help="Agile tracker(eg. mingle) project name" )
+@click.option('--name', prompt='Username',help="Agile tracker(eg. mingle) username" )
+@click.option('--password', prompt=True, hide_input=True, help="Agile tracker(eg.mingle) password")
+def get_release_note(lastcommit,committill,project,name,password):
     commit_log = git.get_log(lastcommit,committill)
     card_nos = parser.parse_log(commit_log)
-    card_details = mingle.get_cards_title("your_first_project", card_nos, "nivedhas", "1qaz!QAZ")
+    card_details = mingle.get_cards_title(project, card_nos, name, password)
     change_log = parser.parse_card_title(card_details)
     click.echo("\n".join(change_log))
--- test/init_test.py
+++ test/init_test.py
@@ -1,17 +1,23 @@
 from unittest import TestCase, main
+
 from click.testing import CliRunner
 from mock import patch
 import releasenote
 from releasenote import git
+from releasenote import mingle
+from releasenote import parser
 
 
 class TestInit(TestCase):
 
     @patch.object(git,'get_log')
-    def test_get_release_note(self,mock_git):
-        mock_git.return_value = "commit_log"
+    @patch.object(parser,'parse_log')
+    @patch.object(mingle,'get_cards_title')
+    @patch.object(parser,'parse_card_title')
+    def test_get_release_note(self,mock_parse_card,mock_mingle,mock_parse_log,mock_git):
+        mock_parse_card.return_value = ["commit_log"]
         runner = CliRunner()
-        result = runner.invoke(releasenote.get_release_note, ['-lc','last_commit','-ct','commit_till'])
+        result = runner.invoke(releasenote.get_release_note, ['-lc','last_commit','-ct','commit_till','--project','project','--name','name','--password','password'])
         self.assertEquals(result.output,'commit_log\n')
 
 if __name__ == "__main__":
