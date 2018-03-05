ee5526ae13309b42533727a47c971fb0868e7e1b
NivedhaSenthil <nivedhasenthil@gmail.com>
2018-03-05 11:29:09 +0530

    Nivedha - Handle repository location exception

--- releasenote/git.py
+++ releasenote/git.py
@@ -1,6 +1,11 @@
 import subprocess
+import sys
 
 def get_log(lastcommit,committill):
-    if(lastcommit):
-        return subprocess.check_output(["git", "log","--oneline",lastcommit+".."+committill])
-    return subprocess.check_output(["git","log","--oneline",committill])
+    try:
+        if(lastcommit):
+            return subprocess.check_output(["git", "log","--oneline",lastcommit+".."+committill])
+        return subprocess.check_output(["git","log","--oneline",committill])
+    except subprocess.CalledProcessError:
+        print "Move into the repository location for which log has to be generated"
+        sys.exit(1)
--- test/git_test.py
+++ test/git_test.py
@@ -1,3 +1,5 @@
+
+import sys
 from mock import patch
 from unittest import TestCase, main
 from releasenote import git
@@ -11,6 +13,13 @@ class TestGit(TestCase):
         commit_log = git.get_log('first_commit', 'second_commit')
         self.assertTrue(mock_co.called)
         self.assertEquals(commit_log,"commit log")
+
+    def test_location_exception(self):
+        with self.assertRaises(SystemExit) as cm:
+            git.get_log('first_commit', 'second_commit')
+        output = sys.stdout.getvalue().strip()
+        self.assertEqual(cm.exception.code, 1)
+        self.assertEquals(output,"Move into the repository location for which log has to be generated")
 
 if __name__ == "__main__":
     main()
